b7220293e6272b2839c49ad956df33bc80b232a5
Matt Kubej <mkubej@gmail.com>
2021-03-26 22:36:29 -0400

    feat: initial commit

---
+++ src/routes/graph/_utils/spidey-db-client/constants.ts
@@ -0,0 +1,16 @@
+export type Data = number | null | string | Data[];
+
+export const CR = '\r';
+export const LF = '\n';
+export const CRLF = `${CR}${LF}`;
+
+export enum Prefix {
+  SimpleString = '+',
+  Error = '-',
+  Integer = ':',
+  BulkString = '$',
+  Array = '*',
+}
+
+export const OK = `${Prefix.SimpleString}OK${CRLF}`;
+export const NULL = `${Prefix.BulkString}-1${CRLF}`;
