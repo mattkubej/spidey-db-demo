59588067d18c50f733a77bdad1329bc31fe638f2
Matt Kubej <mkubej@gmail.com>
2021-03-27 16:57:56 -0400

    refactor: resolve couple type errors

--- src/lib/Graph.svelte
+++ src/lib/Graph.svelte
@@ -1,114 +1,128 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-  import * as d3 from 'd3';
+	import * as d3 from 'd3';
 
 	export let vertices: string[] = [];
 	export let edges: string[] = [];
 
-  const width = 300;
-  const height = 300;
-  let svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
+	const width = 300;
+	const height = 300;
+	let svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
 
-  let nodes = vertices.map(id => ({ id }));
-  let links = edges.map(([source, target]) => ({ source, target }));
+
+  interface Node extends d3.SimulationNodeDatum {
+    id: string;
+  };
+
+  interface Link extends d3.SimulationLinkDatum<Node> {
+		source: string;
+		target: string;
+  };
+
+	let nodes: Node[] = vertices.map((id) => ({ id }));
+	let links: Link[] = edges.map(([source, target]) => ({ source, target }));
+
 
 	onMount(() => {
-    svg = d3.select(".graph")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", `0,0,${width},${height}`);
+		svg = d3
+			.select('.graph')
+			.append('svg')
+			.attr('width', width)
+			.attr('height', height)
+			.attr('viewBox', `0,0,${width},${height}`);
 
-    const link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-      .selectAll("line")
-      .data(links)
-      .join("line");
+		const link = svg
+			.append('g')
+			.attr('stroke', '#999')
+			.attr('stroke-opacity', 0.6)
+			.selectAll('line')
+			.data(links)
+			.join('line');
 
-    const node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-      .selectAll("g")
-      .data(nodes)
-      .enter()
-      .append("g");
+		const node = svg
+			.append('g')
+			.attr('stroke', '#fff')
+			.attr('stroke-width', 1.5)
+			.selectAll('g')
+			.data(nodes)
+			.enter()
+			.append('g');
 
-    node.append("circle")
-      .attr("r", 10)
-      .attr("fill", "red");
+		node.append('circle').attr('r', 10).attr('fill', 'red');
 
-    node.append("text")
-      .text(d => d.id)
-      .attr("cursor", "pointer")
-      .attr("fill", "white")
-      .attr("font-size", 12)
-      .attr("y", 3)
-      .attr("x", -4);
+		node
+			.append('text')
+			.text((d) => d.id)
+			.attr('cursor', 'pointer')
+			.attr('fill', 'white')
+			.attr('font-size', 12)
+			.attr('y', 3)
+			.attr('x', -4);
 
-    function simulationUpdate() {
-      link.attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+		function simulationUpdate() {
+			link
+				.attr('x1', (d) => d.source.x)
+				.attr('y1', (d) => d.source.y)
+				.attr('x2', (d) => d.target.x)
+				.attr('y2', (d) => d.target.y);
 
-      node.attr("transform", d => `translate(${d.x},${d.y})`)
-    }
+			node.attr('transform', (d) => `translate(${d.x},${d.y})`);
+		}
 
-    const simulation = d3.forceSimulation(nodes)
-      .force('link', d3.forceLink(links).id(v => v.id))
-      .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .on('tick', simulationUpdate);
+		const simulation = d3
+			.forceSimulation(nodes)
+			.force(
+				'link',
+				d3.forceLink(links).id((v) => v.id)
+			)
+			.force('charge', d3.forceManyBody())
+			.force('center', d3.forceCenter(width / 2, height / 2))
+			.on('tick', simulationUpdate);
 
-    function dragstarted(event) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      event.subject.fx = event.subject.x;
-      event.subject.fy = event.subject.y;
-    }
+		function dragstarted(event) {
+			if (!event.active) simulation.alphaTarget(0.3).restart();
+			event.subject.fx = event.subject.x;
+			event.subject.fy = event.subject.y;
+		}
 
-    function dragged(event) {
-      event.subject.fx = event.x;
-      event.subject.fy = event.y;
-    }
+		function dragged(event) {
+			event.subject.fx = event.x;
+			event.subject.fy = event.y;
+		}
 
-    function dragended(event) {
-      if (!event.active) simulation.alphaTarget(0);
-      event.subject.fx = null;
-      event.subject.fy = null;
-    }
+		function dragended(event) {
+			if (!event.active) simulation.alphaTarget(0);
+			event.subject.fx = null;
+			event.subject.fy = null;
+		}
 
-    node.call(d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended)
-    );
-  });
+		node.call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended));
+	});
 </script>
 
 <div class="container">
-  <div>
-    <h2>Vertices</h2>
-    <ul>
-      {#each vertices as vertex}
-        <ul>{vertex}</ul>
-      {/each}
-    </ul>
+	<div>
+		<h2>Vertices</h2>
+		<ul>
+			{#each vertices as vertex}
+				<ul>{vertex}</ul>
+			{/each}
+		</ul>
 
-    <h2>Edges</h2>
-    <ul>
-      {#each edges as edge}
-        <ul>{edge}</ul>
-      {/each}
-    </ul>
-  </div>
+		<h2>Edges</h2>
+		<ul>
+			{#each edges as edge}
+				<ul>{edge}</ul>
+			{/each}
+		</ul>
+	</div>
 
-  <div class="graph"></div>
+	<div class="graph" />
 </div>
 
 <style>
-.container {
-  display: flex;
-  justify-content: space-around;
-}
+	.container {
+		display: flex;
+		justify-content: space-around;
+	}
 </style>
