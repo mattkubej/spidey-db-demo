6cc57a4497984612c93cd2fec5fa7ed7d773f29f
Matt Kubej <mkubej@gmail.com>
2021-03-28 22:06:58 -0400

    refactor: only connect to spidey once

--- src/routes/graph/neighbors.json.ts
+++ src/routes/graph/neighbors.json.ts
@@ -1,22 +1,8 @@
-import { SpideyDbClient } from "./_utils/spidey-db-client";
+import spideydb from "$lib/spidey-db";
 import type { Response } from "@sveltejs/kit";
 
 export async function get(): Promise<Response> {
-  const client = new SpideyDbClient();
-  client.connect();
-
-  await client.setVertex('a', '1');
-  await client.setVertex('b', '2');
-  await client.setVertex('c', '3');
-  await client.setVertex('d', '4');
-  await client.setVertex('e', '5');
-
-  await client.setEdge('a', 'b');
-  await client.setEdge('b', 'c');
-  await client.setEdge('c', 'd');
-  await client.setEdge('d', 'e');
-
-  const [vertices, edges] = await client.getNeighbors('a', 3);
+  const [vertices, edges] = await spideydb.getNeighbors('a', 3);
 
   return {
     body: {vertices, edges}
