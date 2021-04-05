ed44ad8a9d6160e45bf832d2ac9b081503902057
Matt Kubej <mkubej@gmail.com>
2021-04-04 21:08:42 -0400

    feat: style graph list

--- src/lib/components/GraphLists.svelte
+++ src/lib/components/GraphLists.svelte
@@ -5,22 +5,40 @@
 
 <div>
   <h2>Vertices</h2>
-  <ul>
-    {#each vertices as vertex}
-      <ul>{vertex}</ul>
-    {/each}
-  </ul>
+  {#if vertices.length > 0}
+    <ul>
+      {#each vertices as vertex}
+        <ul>{vertex}</ul>
+      {/each}
+    </ul>
+  {:else}
+    <p>none</p>
+  {/if}
 
   <h2>Edges</h2>
-  <ul>
-    {#each edges as edge}
-      <ul>{edge}</ul>
-    {/each}
-  </ul>
+  {#if edges.length > 0}
+    <ul>
+      {#each edges as edge}
+        <ul>{edge}</ul>
+      {/each}
+    </ul>
+  {:else}
+    <p>none</p>
+  {/if}
 </div>
 
 <style>
   h2 {
     color: var(--purple);
+  }
+
+  p {
+    color: var(--gray);
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  ul {
+    padding-left: 0px;
   }
 </style>
